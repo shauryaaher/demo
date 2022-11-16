56460d8880fb14a7ee5495ee9121586f80c8dc55
Shaurya <shauryaaher1200@gmail.com>
2022-11-16 14:51:59 +0530

    Add a logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -10,6 +11,13 @@ export default function Home() {
       </Head>
       <header className={styles.default}>
         <nav className={styles.navigator}>
+          <Image
+            className={styles.logoo}
+            src="/logo.png"
+            height={60}
+            width={110}
+            alt="Logo"
+          />
           <ul className={styles.nav_links}>
             <li>
               <Link className={styles.navcontent} href="/otherPage">
--- pages/otherPage.jsx
+++ pages/otherPage.jsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Other.module.css";
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head";
 
 export async function getServerSideProps() {
@@ -20,8 +21,17 @@ export default function otherPage(props) {
       </Head>
       <header className={styles.header}>
         <nav className={styles.nav}>
+          <Image
+            className={styles.logoo}
+            src="/logo.png"
+            height={60}
+            width={110}
+            alt="Logo"
+          />
           <ul className={styles.list}>
-            <li><Link href="/">Home</Link></li>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
           </ul>
         </nav>
       </header>
