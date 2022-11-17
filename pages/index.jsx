087d02022d0568efb06d790c7bd76f461cc206cd
Shaurya Aher <shauryaaher1200@gmail.com>
2022-11-17 15:27:06 +0530

    Refactor

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,65 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+function Fridge() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.container}>
+        <h4>
+          <strong>Chef d&apos;oeuvre</strong>
+        </h4>
+        <p>
+          <em>The same as its name.</em>
+        </p>
+        <Image src="/fridge.jpg" alt="refridgerator" height={250} width={320} />
+      </div>
+    </div>
+  );
+}
+
+function WashingMachine() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.container}>
+        <h4>
+          <strong>Spinner</strong>
+        </h4>
+        <p>
+          <em>
+            An IoT-based machine that you can control from another country.
+          </em>
+        </p>
+        <Image
+          style={{ paddingLeft: "30px", paddingRight: "10px" }}
+          src="/washing-machine.jpg"
+          alt="washing-machine"
+          height={250}
+          width={300}
+        />
+      </div>
+    </div>
+  );
+}
+
+function TV() {
+  return (
+    <Link href="/TV">
+      <div className={styles.card} style={{ width: "380px" }}>
+        <div className={styles.container}>
+          <h4>
+            <strong>Neptune</strong>
+          </h4>
+          <p>
+            <em>Brilliant colors.</em>
+          </p>
+
+          <Image src="/tv.jpg" alt="television" height={250} width={350} />
+        </div>
+      </div>
+    </Link>
+  );
+}
+
 export default function Home() {
   return (
     <>
@@ -20,7 +79,7 @@ export default function Home() {
           />
           <ul className={styles.nav_links}>
             <li>
-              <Link className={styles.navcontent} href="/otherPage">
+              <Link className={styles.navcontent} href="/generalElectronics">
                 General electronics
               </Link>
             </li>
@@ -28,56 +87,9 @@ export default function Home() {
         </nav>
       </header>
       <div className={styles.main}>
-        <div className={styles.card}>
-          <div className={styles.container}>
-            <h4>
-              <strong>Chef d&apos;oeuvre</strong>
-            </h4>
-            <p>
-              <em>The same as its name.</em>
-            </p>
-            <Image
-              src="/fridge.jpg"
-              alt="refridgerator"
-              height={250}
-              width={320}
-            />
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.container}>
-            <h4>
-              <strong>Lock-duck</strong>
-            </h4>
-            <p>
-              <em>Safe. Simple. Reliable.</em>
-            </p>
-            <Image
-              style={{paddingLeft: "30px", paddingRight: "10px"}}
-              src="/washing-machine.jpg"
-              alt="washing-machine"
-              height={250}
-              width={300}
-            />
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.container}>
-            <h4>
-              <strong>Neptune</strong>
-            </h4>
-            <p>
-              <em>Brilliant colors.</em>
-            </p>
-            <Image
-              style={{ paddingLeft: "40px", paddingRight: "70px" }}
-              src="/tv.jpg"
-              alt="television"
-              height={250}
-              width={350}
-            />
-          </div>
-        </div>
+        <TV />
+        <Fridge />
+        <WashingMachine />
       </div>
     </>
   );
