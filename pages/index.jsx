ebb1bffe1d7124aa559a3dae2db25a6c26e55cad
Shaurya Aher <shauryaaher1200@gmail.com>
2022-11-20 13:23:40 +0530

    Delete 3 components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,66 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-function Fridge() {
-  return (
-    <div className={styles.card}>
-      <div className={styles.container}>
-        <h4>
-          <strong>Chef d&apos;oeuvre</strong>
-        </h4>
-        <p>
-          <em>The same as its name.</em>
-        </p>
-        <Image src="/fridge.jpg" alt="refridgerator" height={250} width={320} />
-      </div>
-    </div>
-  );
-}
-
-function WashingMachine() {
-  return (
-    <div className={styles.card}>
-      <div className={styles.container}>
-        <h4>
-          <strong>Spinner</strong>
-        </h4>
-        <p>
-          <em>
-            An IoT-based machine that you can control from another country.
-          </em>
-        </p>
-        <Image
-          style={{ paddingLeft: "30px", paddingRight: "10px" }}
-          src="/washing-machine.jpg"
-          alt="washing-machine"
-          height={250}
-          width={300}
-        />
-      </div>
-    </div>
-  );
-}
-
-function TV() {
-  return (
-    <Link href="/TV">
-      <div className={styles.card} style={{ width: "380px" }}>
-        <div className={styles.container}>
-          <h4>
-            <strong>Neptune</strong>
-          </h4>
-          <p>
-            <em>Brilliant colors.</em>
-          </p>
-
-          <Image src="/tv.jpg" alt="television" height={250} width={350} />
-        </div>
-      </div>
-    </Link>
-  );
-}
+import styles from "../styles/Home.module.scss";
 
 export async function getServerSideProps() {
   const req = await fetch("https://demo-five-ashy.vercel.app/api/hello");
@@ -82,29 +23,79 @@ export default function Home(props) {
         <nav className={styles.navigator}>
           <Image
             className={styles.logoo}
-            src="/logo.png"
-            height={60}
+            src="/emirates.png"
+            height={100}
             width={110}
             alt="Logo"
           />
-          <ul className={styles.nav_links}>
+          <ul className={styles.nav_links} style={{ paddingTop: "20px" }}>
             <li>
-              <Link className={styles.navcontent} href="/general-electronics">
-                General electronics
-              </Link>
-              <Link href="/TV">Neptune</Link>
+              <Link href="/book">Book a flight</Link>
             </li>
           </ul>
         </nav>
       </header>
+
       <center>
         <h1>{props.heading}</h1>
       </center>
-      <div className={styles.main}>
-        <TV />
-        <Fridge />
-        <WashingMachine />
+      <div className={styles.card}>
+        <div className={styles.container}>
+          <h4>
+            <strong>Special offer</strong>
+          </h4>
+          <p>
+            <em>
+              Get 2 nights at the prestigious Burj Al Arab hotel in Dubai and a
+              tour @ INR. 100,000.
+            </em>
+            <Image
+              src="/hotel.jpg"
+              alt="burj-al-arab"
+              height={270}
+              width={320}
+              style={{ borderRadius: "7px" }}
+            />
+          </p>
+        </div>
       </div>
+      <div className={styles.card}>
+        <div className={styles.container}>
+          <h4>
+            <strong>Special offer</strong>
+          </h4>
+          <p>
+            <em>
+              Apply for the Emirates Platinum Card and get extra meals and
+              upgrades on your next flight.
+            </em>
+          </p>
+          <Image
+            src="/food.jpg"
+            alt="fried-rice"
+            height={300}
+            width={320}
+            style={{ borderRadius: "7px" }}
+          />
+        </div>
+      </div>
+      <Link href="/book">
+        <div className={styles.card} style={{ cursor: "pointer" }}>
+          <div className={styles.container}>
+            <h4>Quick flights</h4>
+          </div>
+          <p style={{ paddingLeft: "13px" }}>
+            <em>Check out the quick flight bookings.</em>
+          </p>
+          <Image
+            src="/plane.jpg"
+            alt="airplane-emirates"
+            height={200}
+            width={349}
+            style={{ borderRadius: "7px" }}
+          />
+        </div>
+      </Link>
     </>
   );
 }
