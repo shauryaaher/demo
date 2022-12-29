b8b04e707be2bdb0d4bf7cacbfa07f470809abc9
Shaurya Aher <shauryaaher1200@gmail.com>
2022-12-29 14:30:13 +0530

    Adjustible quantity for /api/bom_to_lon

--- pages/api/bom_to_lon.js
+++ pages/api/bom_to_lon.js
@@ -1,22 +1,20 @@
 const stripe = require("stripe")(
-    "sk_test_51Kv2S3SJMBnIhuo7iZ3paAmDthweFE9Qqop8C9tpQclh4lSCWxKBUCiB349UpHwI5yS9F9QLOPrQHKjD1i88D4p1004pOkrlMR"
-  );
-  
-  export default async function handlePayments(req, res) {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price: "price_1M687cSJMBnIhuo7PF0tCqAE",
-          quantity: 1,
-        },
-      ],
-      submit_type: "book",
-      mode: "payment",
-      success_url:
-        "https://demo-five-ashy.vercel.app/success",
-      cancel_url:
-        "http://demo-five-ashy.vercel.app/failure",
-    });
-    res.redirect(301, session.url);
-  }
-  
+  "sk_test_51Kv2S3SJMBnIhuo7iZ3paAmDthweFE9Qqop8C9tpQclh4lSCWxKBUCiB349UpHwI5yS9F9QLOPrQHKjD1i88D4p1004pOkrlMR"
+);
+
+export default async function handlePayments(req, res) {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: "price_1M687cSJMBnIhuo7PF0tCqAE",
+        quantity: 1,
+        adjustible_quantity: { enabled: true, minimum: 1, maximum: 4 },
+      },
+    ],
+    submit_type: "book",
+    mode: "payment",
+    success_url: "https://demo-five-ashy.vercel.app/success",
+    cancel_url: "http://demo-five-ashy.vercel.app/failure",
+  });
+  res.redirect(301, session.url);
+}
