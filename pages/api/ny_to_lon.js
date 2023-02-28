aa5d1cae274c440bb350a41f1f73c4dc326a3031
Shaurya Aher <shauryaaher1200@gmail.com>
2023-02-28 12:30:53 +0530

    Use the Edge runtime to check if it gives performance improvements

--- pages/api/ny_to_lon.js
+++ pages/api/ny_to_lon.js
@@ -2,6 +2,12 @@ const stripe = require("stripe")(
   "sk_test_51Kv2S3SJMBnIhuo7iZ3paAmDthweFE9Qqop8C9tpQclh4lSCWxKBUCiB349UpHwI5yS9F9QLOPrQHKjD1i88D4p1004pOkrlMR"
 );
 
+import { NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
 export default async function handlePayments(req, res) {
   const session = await stripe.checkout.sessions.create({
     submit_type: "book",
@@ -18,5 +24,5 @@ export default async function handlePayments(req, res) {
     success_url: "https://demo-five-ashy.vercel.app/success",
     cancel_url: "http://demo-five-ashy.vercel.app/failure",
   });
-  res.redirect(301, session.url);
+  return NextResponse.redirect(session.url);
 }
