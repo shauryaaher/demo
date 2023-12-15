329fdba4bf182d61e80126befe0cb3bf3830a50d
Shaurya Aher <shauryaaher1200@gmail.com>
2023-12-15 21:40:54 +0530

    Remove the useless check

--- test/hello.test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-
-describe("hello.js response", () => {
-  it("Check if hello.js returns the correct response.", async () => {
-    const res = await request("https://demo-five-ashy.vercel.app").get("/hello");
-    const data = res.body;
-    expect(data).toBe({"data": "Hello world!"});
-  });
-});
