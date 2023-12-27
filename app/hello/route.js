2259b46d5f9e2849d98881a0eaabba562cd126c7
Shaurya Aher <shauryaaher1200@gmail.com>
2023-12-27 05:09:23 +0000

    Remove unwanted route

--- app/hello/route.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextResponse } from "next/server";
-
-export function GET(req, res) {
-  return NextResponse.json({"data": "Hello world!"});
-}
